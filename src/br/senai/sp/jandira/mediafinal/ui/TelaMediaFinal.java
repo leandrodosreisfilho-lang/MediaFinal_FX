b2c721e8101e06a0ac9e6f1917fcbabb4155be3b
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2025-10-27 17:16:46 -0300

    Media final visual funcional

--- src/br/senai/sp/jandira/mediafinal/ui/TelaMediaFinal.java
+++ src/br/senai/sp/jandira/mediafinal/ui/TelaMediaFinal.java
@@ -4,21 +4,28 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.Optional;
+
 public class TelaMediaFinal extends Application {
 
 
+    VBox painelResultado;
+    TextField tfNomeAluno;
+    TextField tfNota1;
+    TextField tfNota2;
+    TextField tfNota3;
+    TextField tfNota4;
+
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setWidth(400);
-        stage.setHeight(400);
+        stage.setWidth(600);
+        stage.setHeight(600);
         stage.setResizable(false);
         stage.setTitle("Media Final");
 
@@ -32,23 +39,24 @@ public class TelaMediaFinal extends Application {
         titulo.setPadding(new Insets(10,0,10,10));
 
         HBox painelSuperior = new HBox();
-        painelSuperior.setStyle("-fx-background-color: red");
+        painelSuperior.setStyle("-fx-background-color: #f700ff");
         painelSuperior.getChildren().addAll(titulo);
 
         //criação do formulario
         VBox painelFormulario = new VBox();
 
-        painelFormulario.setPadding(new Insets(10,10,0,10));
+        painelFormulario.setPadding(new Insets(85,10,0,10));
+        painelFormulario.setStyle("-fx-background-color: #B75EECFF");
         Label lblNomeAluno = new Label("Nome do Aluno:");
         Label lblNota1 = new Label("Nota 1:");
         Label lblNota2 = new Label("Nota 2:");
         Label lblNota3 = new Label("Nota 3:");
         Label lblNota4 = new Label("Nota 4:");
-        TextField tfNomeAluno = new TextField();
-        TextField tfNota1 = new TextField();
-        TextField tfNota2 = new TextField();
-        TextField tfNota3 = new TextField();
-        TextField tfNota4 = new TextField();
+        tfNomeAluno = new TextField();
+        tfNota1 = new TextField();
+        tfNota2 = new TextField();
+        tfNota3 = new TextField();
+        tfNota4 = new TextField();
         painelFormulario.getChildren().addAll(
                 lblNomeAluno,tfNomeAluno,
                 lblNota1,tfNota1,
@@ -61,7 +69,7 @@ public class TelaMediaFinal extends Application {
         painelBotoes.setPadding(new Insets(0,10,0,0));
         painelBotoes.setSpacing(15);
         painelBotoes.setAlignment(Pos.CENTER);
-        //painelBotoes.setStyle("-fx-background-color: red");
+        painelBotoes.setStyle("-fx-background-color: #b75eec");
         Button btCalcularMedia = new Button("Calcular Média");
         btCalcularMedia.setPrefWidth(150);
         btCalcularMedia.setPrefHeight(70);
@@ -81,12 +89,17 @@ public class TelaMediaFinal extends Application {
                 btSair
         );
 
-        VBox painelResultado = new VBox();
-        painelResultado.setStyle("-fx-background-color: red");
-        Label lblResultados = new Label("Resultados");
-        Label lblNomeResultado = new Label("Nome do aluno");
-        Label lblMediaFinal = new Label("Media Final");
-        Label lblSituacao = new Label("Situação");
+        painelResultado = new VBox();
+        painelResultado.setPadding(new Insets(15));
+        painelResultado.setStyle("-fx-background-color: #f700ff");
+        Label lblResultados = new Label("Resultados: ");
+        lblResultados.setStyle("-fx-font-size: 22; -fx-text-fill: white; -fx-font-weight: bold;");
+        Label lblNomeResultado = new Label("Nome do aluno: ");
+        lblNomeResultado.setStyle("-fx-text-fill: white;");
+        Label lblMediaFinal = new Label("Media Final: ");
+        lblMediaFinal.setStyle("-fx-text-fill: white;");
+        Label lblSituacao = new Label("Situação: ");
+        lblSituacao.setStyle("-fx-text-fill: white;");
         painelResultado.getChildren().addAll(
                 lblResultados,lblNomeResultado,
                 lblMediaFinal,lblSituacao
@@ -105,7 +118,107 @@ public class TelaMediaFinal extends Application {
 
         // colocar a cena no stage
         stage.setScene(scene);
-
         stage.show();
+
+        //interceptar cliques nos botões
+        btCalcularMedia.setOnAction(e -> {
+            if (validarEntrada()){
+                String nomeAluno = tfNomeAluno.getText();
+                lblNomeResultado.setText("Nome do aluno: " + nomeAluno);
+                System.out.println("Nome do aluno: " + nomeAluno);
+
+                String nota1 = tfNota1.getText();
+                String nota2 = tfNota2.getText();
+                String nota3 = tfNota3.getText();
+                String nota4 = tfNota4.getText();
+
+                double media = calcularMedia(nota1, nota2, nota3, nota4);
+                String mediaFormatada = String.format("%.2f", media);
+                lblMediaFinal.setText("Media Final: " + mediaFormatada);
+
+                String situacao = definirSituacao(media);
+                lblSituacao.setText("Situação: " + situacao);
+
+            }
+
+        });
+
+        btLimpar.setOnAction(e -> {
+            tfNomeAluno.setText("");
+            tfNota1.setText("");
+            tfNota2.setText("");
+            tfNota3.setText("");
+            tfNota4.setText("");
+            lblNomeResultado.setText("Nome do aluno: ");
+            lblMediaFinal.setText("Media Final: ");
+            lblSituacao.setText("Situacao: ");
+            painelResultado.setStyle("-fx-background-color: #f700ff");
+            tfNomeAluno.requestFocus();
+
+        });
+
+        btSair.setOnAction(e -> {
+            Alert alerta = new Alert(Alert.AlertType.CONFIRMATION, "Sair do programa?", ButtonType.YES, ButtonType.NO);
+            Optional<ButtonType> resposta = alerta.showAndWait();
+
+            if (resposta.get() == ButtonType.YES) {
+                System.exit(0);
+            }
+
+        });
+
+    }
+
+    private double calcularMedia(String n1, String n2, String n3, String n4) {
+        //parseDoubleconverte string em double
+        double nota1 = Double.parseDouble(n1);
+        double nota2 = Double.parseDouble(n2);
+        double nota3 = Double.parseDouble(n3);
+        double nota4 = Double.parseDouble(n4);
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        return media;
+    }
+
+    private String definirSituacao(double media) {
+        if (media < 4) {
+            painelResultado.setStyle("-fx-background-color: #ff0000");
+            return "Reprovado";
+        }else if (media >= 6.0) {
+            painelResultado.setStyle("-fx-background-color: #1aff00");
+            return "Aprovado";
+        } else {
+            painelResultado.setStyle("-fx-background-color: #ff8000");
+            return "Recuperação";
+
+        }
+    }
+    private boolean validarEntrada(){
+        if (tfNomeAluno.getText().isEmpty()){
+            mostrarMensagem(Alert.AlertType.ERROR, "Preencha o nome do aluno!");
+            tfNomeAluno.requestFocus();
+            return false;
+        } else if (tfNota1.getText().isEmpty()){
+            mostrarMensagem(Alert.AlertType.ERROR, "Preencha nota 1 do aluno!");
+            tfNota1.requestFocus();
+            return false;
+        } else if (tfNota2.getText().isEmpty()){
+            mostrarMensagem(Alert.AlertType.ERROR, "Preencha nota 2 do aluno!");
+            tfNota2.requestFocus();
+            return false;
+        }else if (tfNota3.getText().isEmpty()){
+            mostrarMensagem(Alert.AlertType.ERROR, "Preencha nota 3 do aluno!");
+            tfNota3.requestFocus();
+            return false;
+        }else if (tfNota4.getText().isEmpty()){
+            mostrarMensagem(Alert.AlertType.ERROR, "Preencha nota 4 do aluno!");
+            tfNota4.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
+    private void mostrarMensagem(Alert.AlertType tipo, String mensagem) {
+        Alert alerta = new Alert(tipo, mensagem);
+        alerta.showAndWait();
     }
 }
